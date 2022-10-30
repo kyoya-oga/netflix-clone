56512fb10976c93f1c90756dfa2baf1e21d66765
Ofek Kazes <ofekkazes2@gmail.com>
2022-10-30 15:40:06 +0200

    Changed API keys

--- firebase.ts
+++ firebase.ts
@@ -7,12 +7,13 @@ import { getAuth } from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyBATdXNbXuAZjh-_M20r2MLIRwOXQ8O0-U',
-  authDomain: 'next-netflix-clone-4ae85.firebaseapp.com',
-  projectId: 'next-netflix-clone-4ae85',
-  storageBucket: 'next-netflix-clone-4ae85.appspot.com',
-  messagingSenderId: '418366388830',
-  appId: '1:418366388830:web:7fba7062230ced026792cf',
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
